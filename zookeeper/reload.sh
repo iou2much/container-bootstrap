42b38654a8aa8ed45e84674275ff476b24cc6aaa
iou2much <iou2much@sina.com>
2016-07-17 09:19:17 +0000

    zookeeper startup script fixed

--- zookeeper/reload.sh
+++ zookeeper/reload.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
+while true
+do
+  if mkdir /tmp/zk.lock 2>/dev/null; then
+
+#  if [ -f /tmp/zk.lock ] ;
+    break
+  else
+    echo 'waiting for lock..' >> /tmp.log
+    sleep 1
+  fi
+done
+#touch /tmp/zk.lock
+
 cat /usr/local/zookeeper/conf/zoo.cfg |sed "s/`hostname`/0.0.0.0/g" > /usr/local/zookeeper/conf/zoo.cfg
-if [ -f /tmp/zookeeper/zkdata/zookeeper_server.pid ] ; 
+
+if [ -f /tmp/zookeeper/zkdata/zookeeper_server.pid ] ;
 then
-    kill -9 `cat /tmp/zookeeper/zkdata/zookeeper_server.pid`
+    kill -9 `cat /tmp/zookeeper/zkdata/zookeeper_server.pid` 2>/dev/null
+    kill -9 `jps|awk '{print $1}'` 2>/dev/null
+    echo 'killed zk process'>> /tmp.log
 fi
+
+port_occupy=`netstat -anp|grep 2181|wc -l`
+while [ $port_occupy -neq 0 ]
+do
+  echo 'waiting for port available'>> /tmp.log
+  port_occupy=`netstat -anp|grep 2181|wc -l`
+  sleep 1
+done
+
+rm /tmp/zookeeper/zkdata/version-2/*
 /usr/local/zookeeper/bin/zkServer.sh restart
+
+sleep 5
+rmdir /tmp/zk.lock
--- zookeeper/start-zk.sh
+++ zookeeper/start-zk.sh
@@ -5,29 +5,29 @@ is_exist=0
 
 `etcdctl2 ls /zk/hosts/${hostname} > /dev/null 2>&1 ` && is_exist=1
 
-if [ $is_exist -eq 1 ] ; 
+if [ $is_exist -eq 1 ] ;
 then
   echo 'hostname already exists!!!!!!!!'
-  confd -confdir=/bootstrap/zookeeper/confd/ -watch=true -onetime=false -backend etcd -node http://etcd.svc:2379 & 
-  /bootstrap/zookeeper/reload
-  #/usr/local/zookeeper/bin/zkServer.sh start-foreground 
+  confd -confdir=/bootstrap/zookeeper/confd/ -watch=true -onetime=false -backend etcd -node http://etcd.svc:2379 &
+  /bootstrap/zookeeper/reload.sh
+  #/usr/local/zookeeper/bin/zkServer.sh start-foreground
 else
-  
+
   count=`etcdctl2 ls /zk/hosts 2> /dev/null | wc -l`
   id=$(($count+1))
-  
+
   echo $count
 
-  confd -confdir=/bootstrap/zookeeper/confd/ -watch=true -onetime=false -backend etcd -node http://etcd.svc:2379 &
-
-  etcdctl2 set /zk/hosts/${hostname} '{"id":"'${id}'","hostname":"'${hostname}'"}'
-  
   rm -rf /tmp/zookeeper/ 2> /dev/null
   mkdir -p /tmp/zookeeper/logs /tmp/zookeeper/zkdata
   echo "${id}" > /tmp/zookeeper/zkdata/myid
-  
+
+  confd -confdir=/bootstrap/zookeeper/confd/ -watch=true -onetime=false -backend etcd -node http://etcd.svc:2379 &
+  sleep 5
+
+  etcdctl2 set /zk/hosts/${hostname} '{"id":"'${id}'","hostname":"'${hostname}'"}'
   etcdctl2 set /trackdown/${hostname} "/zk/hosts/${hostname}"
-  
+
 fi
 
 
